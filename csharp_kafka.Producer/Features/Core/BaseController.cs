1c1cb214472ec7a0efc805abd41e874e2682bf13
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-09 23:05:20 +0630

    -

--- csharp_kafka.Producer/Features/Core/BaseController.cs
+++ csharp_kafka.Producer/Features/Core/BaseController.cs
@@ -1,5 +1,5 @@
-﻿using csharp_kafka.Producer.Extensions;
-using Microsoft.AspNetCore.Mvc;
+﻿global using csharp_kafka.Producer.Extensions;
+global using Microsoft.AspNetCore.Mvc;
 
 namespace csharp_kafka.Producer.Features.Core;
 
