bfaa1284886cc97b44ed2b5defd10ed51f68b1a3
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-09 08:34:25 +0630

    -

--- csharp_kafka.Consumer/Configurations/AppSetting.cs
+++ csharp_kafka.Consumer/Configurations/AppSetting.cs
@@ -1,24 +1,23 @@
-﻿namespace csharp_kafka.Consumer.Configurations
+﻿namespace csharp_kafka.Consumer.Configurations;
+
+public class AppSetting
 {
-    public class AppSetting
-    {
-        public Logging Logging { get; set; }
-        public Kafka Kafka { get; set; }
-    }
+    public Logging Logging { get; set; }
+    public Kafka Kafka { get; set; }
+}
 
-    public class Logging
-    {
-        public Loglevel LogLevel { get; set; }
-    }
+public class Logging
+{
+    public Loglevel LogLevel { get; set; }
+}
 
-    public class Loglevel
-    {
-        public string Default { get; set; }
-        public string MicrosoftAspNetCore { get; set; }
-    }
+public class Loglevel
+{
+    public string Default { get; set; }
+    public string MicrosoftAspNetCore { get; set; }
+}
 
-    public class Kafka
-    {
-        public string BootstrapServers { get; set; }
-    }
+public class Kafka
+{
+    public string BootstrapServers { get; set; }
 }
