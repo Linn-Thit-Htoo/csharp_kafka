0a0249e9f0aeb24aad4f2422a7536b3ac742a66f
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-09 23:02:38 +0630

    -

--- csharp_kafka.Producer/Extensions/Extension.cs
+++ csharp_kafka.Producer/Extensions/Extension.cs
@@ -4,5 +4,6 @@ namespace csharp_kafka.Producer.Extensions;
 
 public static class Extension
 {
-    public static string ToJson(this object obj) => JsonConvert.SerializeObject(obj, Formatting.Indented);
+    public static string ToJson(this object obj) =>
+        JsonConvert.SerializeObject(obj, Formatting.Indented);
 }
