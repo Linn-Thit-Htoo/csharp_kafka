01565efecdb90d43e2211b280abb5543c3549d64
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-09 23:02:35 +0630

    -

--- csharp_kafka.Producer/Extensions/Extension.cs
+++ csharp_kafka.Producer/Extensions/Extension.cs
@@ -1,9 +1,8 @@
 ﻿using Newtonsoft.Json;
 
-namespace csharp_kafka.Producer.Extensions
+namespace csharp_kafka.Producer.Extensions;
+
+public static class Extension
 {
-    public static class Extension
-    {
-        public static string ToJson(this object obj) => JsonConvert.SerializeObject(obj, Formatting.Indented);
-    }
+    public static string ToJson(this object obj) => JsonConvert.SerializeObject(obj, Formatting.Indented);
 }
