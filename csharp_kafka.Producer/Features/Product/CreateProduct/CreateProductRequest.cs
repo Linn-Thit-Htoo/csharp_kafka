ecd83651af356efe744dfac5b03d052f90ec6d23
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-09 23:04:13 +0630

    -

--- csharp_kafka.Producer/Features/Product/CreateProduct/CreateProductRequest.cs
+++ csharp_kafka.Producer/Features/Product/CreateProduct/CreateProductRequest.cs
@@ -1,8 +1,7 @@
-﻿namespace csharp_kafka.Producer.Features.Product.CreateProduct
+﻿namespace csharp_kafka.Producer.Features.Product.CreateProduct;
+
+public class CreateProductRequest
 {
-    public class CreateProductRequest
-    {
-        public string ProductName { get; set; }
-        public decimal Price { get; set; }
-    }
+    public string ProductName { get; set; }
+    public decimal Price { get; set; }
 }
