f8f6dbd3d9aee5bfdf9f4c81a608e90b7cad2a46
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-09 08:34:10 +0630

    -

--- csharp_kafka.Producer/Features/Product/CreateProduct/CreateProductService.cs
+++ csharp_kafka.Producer/Features/Product/CreateProduct/CreateProductService.cs
@@ -2,39 +2,38 @@
 using csharp_kafka.Producer.Configurations;
 using Microsoft.Extensions.Options;
 
-namespace csharp_kafka.Producer.Features.Product.CreateProduct
+namespace csharp_kafka.Producer.Features.Product.CreateProduct;
+
+public class CreateProductService
 {
-    public class CreateProductService
+    private readonly AppSetting _appSetting;
+
+    private readonly IProducer<Null, string> _producer;
+
+    public CreateProductService(IOptions<AppSetting> setting)
     {
-        private readonly AppSetting _appSetting;
+        _appSetting = setting.Value;
 
-        private readonly IProducer<Null, string> _producer;
-
-        public CreateProductService(IOptions<AppSetting> setting)
+        var producerconfig = new ProducerConfig
         {
-            _appSetting = setting.Value;
+            BootstrapServers = _appSetting.Kafka.BootstrapServers,
+            EnableIdempotence = true
+        };
 
-            var producerconfig = new ProducerConfig
-            {
-                BootstrapServers = _appSetting.Kafka.BootstrapServers,
-                EnableIdempotence = true
-            };
+        _producer = new ProducerBuilder<Null, string>(producerconfig).Build();
+    }
 
-            _producer = new ProducerBuilder<Null, string>(producerconfig).Build();
+    public async Task ProduceAsync(string topic, string message)
+    {
+        try
+        {
+            var kafkamessage = new Message<Null, string> { Value = message, };
+
+            await _producer.ProduceAsync(topic, kafkamessage);
         }
-
-        public async Task ProduceAsync(string topic, string message)
+        catch (Exception ex)
         {
-            try
-            {
-                var kafkamessage = new Message<Null, string> { Value = message, };
-
-                await _producer.ProduceAsync(topic, kafkamessage);
-            }
-            catch (Exception ex)
-            {
-                throw;
-            }
+            throw;
         }
     }
 }
