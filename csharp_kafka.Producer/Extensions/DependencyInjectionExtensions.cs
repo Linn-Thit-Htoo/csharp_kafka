3488e85c85fd3211184fd37b0c3c58623f4caf07
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-09 23:02:26 +0630

    -

--- csharp_kafka.Producer/Extensions/DependencyInjectionExtensions.cs
+++ csharp_kafka.Producer/Extensions/DependencyInjectionExtensions.cs
@@ -1,27 +1,26 @@
 ﻿using csharp_kafka.Producer.Configurations;
 using csharp_kafka.Producer.Features.Product.CreateProduct;
 
-namespace csharp_kafka.Producer.Extensions
+namespace csharp_kafka.Producer.Extensions;
+
+public static class DependencyInjectionExtensions
 {
-    public static class DependencyInjectionExtensions
+    public static IServiceCollection AddDependencies(this IServiceCollection services, WebApplicationBuilder builder)
     {
-        public static IServiceCollection AddDependencies(this IServiceCollection services, WebApplicationBuilder builder)
+        builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
+            .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables();
+
+        builder.Services.AddControllers().AddJsonOptions(opt =>
         {
-            builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
-                .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
-                .AddEnvironmentVariables();
+            opt.JsonSerializerOptions.PropertyNamingPolicy = null;
+        });
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddSingleton<CreateProductService>();
+        builder.Services.AddHealthChecks();
+        builder.Services.Configure<AppSetting>(builder.Configuration);
 
-            builder.Services.AddControllers().AddJsonOptions(opt =>
-            {
-                opt.JsonSerializerOptions.PropertyNamingPolicy = null;
-            });
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-            builder.Services.AddSingleton<CreateProductService>();
-            builder.Services.AddHealthChecks();
-            builder.Services.Configure<AppSetting>(builder.Configuration);
-
-            return services;
-        }
+        return services;
     }
 }
